83402a077fac4fd72b6634e1bcca47fb29872d05
Diego Sogari <diego.sogari@gmail.com>
2025-04-19 22:24:02 -0300

    Add format parsing flag in examples

--- packages/tsargp/src/examples/calc.ts
+++ packages/tsargp/src/examples/calc.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env bun
-import { parse, handleError } from 'tsargp';
+import { format, handleError, parse } from 'tsargp';
 import options from './calc.options.js';
 
 try {
-  const values = await parse(options);
+  const values = await parse(options, undefined, { format });
   const result = values.add ?? values.sub ?? values.mult ?? values.div ?? NaN;
   console.log(result);
 } catch (err) {
